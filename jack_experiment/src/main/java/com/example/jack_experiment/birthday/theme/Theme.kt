07486bf15346a75a6bb94b1e83a0b6853ab3dc5c
Viachaslau Mazhaliuk <slavavmzk@gmail.com>
2023-12-03 19:52:36 -0500

    code inspection adjustments

--- jack_experiment/src/main/java/com/example/jack_experiment/birthday/theme/Theme.kt
+++ jack_experiment/src/main/java/com/example/jack_experiment/birthday/theme/Theme.kt
@@ -1,3 +1,7 @@
+@file:Suppress("PrivatePropertyName", "PrivatePropertyName", "PrivatePropertyName",
+    "PrivatePropertyName", "PrivatePropertyName", "PrivatePropertyName"
+)
+
 package com.example.jack_experiment.birthday.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,6 +11,9 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 
+@Suppress("PrivatePropertyName", "PrivatePropertyName", "PrivatePropertyName",
+    "PrivatePropertyName"
+)
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
